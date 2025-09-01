6f75c2a39f9e7df0d8da6437971a647d706a6d80
Seoyeon <yeonnnn282@gmail.com>
2025-09-01 10:47:42 +0900

    .

--- chap02/src/sec04/exam02/KeyCodeExample.java
+++ chap02/src/sec04/exam02/KeyCodeExample.java
@@ -4,14 +4,14 @@ public class KeyCodeExample {
 	public static void main(String[] args) throws Exception {
 		int keyCode;
 		
-		keyCode = System.console().read();
-		System.out.println("keyCode: " + keyCode);
-		
 		keyCode = System.in.read();
 		System.out.println("keyCode: " + keyCode);
 		
 		keyCode = System.in.read();
-		System.out.println("keyCode " + keyCode);
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
 	}
 
 }
