f2e816177e6765c2576b7b61a97d443668c70284
Seoyeon <yeonnnn282@gmail.com>
2025-09-10 11:31:57 +0900

    9/10

---
+++ chap06/src/sec02/exam02/FieldnitValueExample.java
@@ -0,0 +1,20 @@
+package sec02.exam02;
+
+public class FieldnitValueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteField: " + fiv.byteField);
+		System.out.println("shortField: " + fiv.shortField);
+		System.out.println("intField: " + fiv.intField);
+		System.out.println("boolean: " + fiv.booleanField);
+		System.out.println("char: " + fiv.charField);
+		System.out.println("double: " + fiv.doubleField);
+		System.out.println("arrField: " + fiv.arrField);
+		System.out.println("reference: " + fiv.referenceField);
+
+	}
+
+}
---
+++ cote/src/cote/exam1330/Main.java
@@ -0,0 +1,15 @@
+package cote.exam1330;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		
+
+	}
+
+}
