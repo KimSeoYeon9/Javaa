e10689494fa395c69cdbf956c2ffa2d21eb9c149
Seoyeon <yeonnnn282@gmail.com>
2025-09-17 12:09:05 +0900

    9/17

---
+++ chap06/src/sec4/exma06/Calculator.java
@@ -0,0 +1,12 @@
+package sec4.exma06;
+
+public class Calculator {
+	
+	double areaRectangle(double width) {
+		return width * width;
+		}
+
+	double areaRectangle(double width, double height) {
+		return width * height;
+		}
+}
---
+++ chap06/src/sec5/exam04/Singleton.java
@@ -0,0 +1,12 @@
+package sec5.exam04;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+
+}
