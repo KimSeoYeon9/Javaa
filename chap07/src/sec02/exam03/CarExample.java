c06f77bb5600e801568a674f97f5886bd1cb5dd3
Seoyeon <yeonnnn282@gmail.com>
2025-10-13 14:22:51 +0900

    10/13

---
+++ chap07/src/sec02/exam03/CarExample.java
@@ -0,0 +1,28 @@
+package sec02.exam03;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		for(int i=1; i<=5; i++) {
+			int problemLocation = car.run();
+			switch(problemLocation) {
+			case 1:
+				System.out.println("앞왼쪽 HankookTire로 교체.");
+				car.frontLeftTire = new KumhoTire("뒤오른쪽.", 17);
+			case 2:
+				System.out.println("앞오른쪽 HankookTire로 교체");
+				car.frontRightTire = new KumhoTire("뒤오른쪽.", 17);
+			case 3: 
+				System.out.println("뒤왼쪽 HankookTire로 교체");
+				car.backLeftTire = new KumhoTire("뒤오른쪽.", 17);
+			case 4:
+				System.out.println("뒤오른쪽 HankookTire로 교체");
+				car.backRightTire = new KumhoTire("뒤오른쪽.", 17);
+				break;
+			}
+			System.out.println("----------------------------------");
+		}
+	}
+
+}
