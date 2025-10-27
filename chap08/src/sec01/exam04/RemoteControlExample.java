701e5d6a2fccfd6c3abbf85265d3d25dcc8f7e65
Seoyeon <yeonnnn282@gmail.com>
2025-10-27 12:22:56 +0900

    10/27

---
+++ chap08/src/sec01/exam04/RemoteControlExample.java
@@ -0,0 +1,17 @@
+package sec01.exam04;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc;
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+
+	}
+
+}
---
+++ chap08/src/sec01/exam05/Searchable.java
@@ -0,0 +1,4 @@
+package sec01.exam05;
+public interface Searchable {
+void search(String url);
+}
